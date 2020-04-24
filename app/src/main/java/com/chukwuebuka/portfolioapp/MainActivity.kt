9fdbd0c9e5b77a0ca0f2fe0e60ab1ab115006a31
Chukwuebuka <muofunanya3@gmail.com>
2020-04-24 15:11:19 +0100

    Added extra functionality like the onClickListener

--- app/src/main/java/com/chukwuebuka/portfolioapp/MainActivity.kt
+++ app/src/main/java/com/chukwuebuka/portfolioapp/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.chukwuebuka.portfolioapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.ActionMode
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    var TAG: String= "ReadActivity"
-    imageView: ImageView
+    val TAG: String= "ReadActivity"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +18,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
+        val imageView = findViewById<ImageView?>(R.id.profilepic)
+        imageView?.setOnClickListener {
+            Log.d(TAG, "Profile Picture clicked ")
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 
+    override fun onClick(v: View?) {
+        Log.d(TAG, "Entered onClick function")
+        val intent = Intent(Intent.ACTION_SEND)
+        val message = "Message"
+        intent.putExtra(Intent.EXTRA_TEXT, message)
+        intent.type= "text/plain"
+        val chooser = Intent.createChooser(intent, "Open with :")
+        if(intent.resolveActivity(packageManager)!= null){
+            startActivity(chooser)
+        }
+    }
 
     override fun onActionModeStarted(mode: android.view.ActionMode?) {
         Log.d(TAG, "The activity is in onStart state")
